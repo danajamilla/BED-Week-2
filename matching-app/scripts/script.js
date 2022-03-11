ca4fde356d464588c04e9fb7c5abd3e17b17d59e
danajamilla <danajamilla@hotmail.com>
2022-03-11 15:01:21 +0100

    Wiki bijgewerkt

---
+++ matching-app/scripts/script.js
@@ -0,0 +1,10 @@
+// /******************** 
+//  HAMBURGERMENU
+//  ********************/
+// var hamburgerMenu = document.querySelector("nav button:nth-of-type(3)");
+
+// hamburgerMenu.addEventListener("click", toggleHamburgerMenu);
+// function toggleHamburgerMenu() {
+//     var navElement = document.querySelector("nav");
+//     navElement.classList.toggle("menuOpen");
+// }
