4aa4e62e6166542836bac22b07ef20af0e8feb0b
danajamilla <danajamilla@hotmail.com>
2022-03-14 16:15:05 +0100

    code update

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3002
 
 app.set("view engine", "ejs")
 
 app.get('/', (req, res) => {
   res.render('index')
+})
+
+app.get('/matchpagina', (req, res) => {
+  res.render('matchpagina')
 })
 
 app.use(express.static("static"))
